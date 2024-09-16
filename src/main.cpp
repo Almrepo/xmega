1a793185b7550d48dc8ee419cc624c916d3c59ed
Alexandr Malygin <sambox@bk.ru>
2024-09-16 07:53:22 +0300

    clear main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 
 int main()
 {
-    XmegaClock clock(XmegaClock::XMEGA_TYPE_OS_IN, XmegaClock::XMEGA_OS_HZ_32);
-    XmegaUsart usart(&USARTC0, &PORTC, PIN1_bm, PIN0_bm, 9600, 320000);
-    PORTC.DIRSET = PIN0_bm;
 
     while (1)
     {
