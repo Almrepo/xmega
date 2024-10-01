c03b5b23099d991594e3f1dfffea1411bbab897a
Alexandr Malygin <sambox@bk.ru>
2024-10-01 10:22:56 +0300

    edit

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,11 @@ int main()
 {
     XmegaClock clock(XmegaClock::XMEGA_TYPE_OS_IN, XmegaClock::XMEGA_OS_HZ_32);
     XmegaIntLvl intlvl(XmegaIntLvl::XMEGA_INT_LVL_ALL_EN);
-    XmegaAdc adc;
-    //adc.xmega_adc_init(&ADCA,A);
+    XmegaAdc adc(&ADCA, ADC_REFSEL_AREFA_gc, ADC_PRESCALER_DIV128_gc,&ADCA.CH0 );
+  
 
     while (1)
     {
-        
-       
     }
 
     return 0;
--- src/xmega_adc.cpp
+++ src/xmega_adc.cpp
@@ -8,42 +8,44 @@ void XmegaAdc::xmega_adc_clock()
     OSC.PLLCTRL = OSC_PLLSRC_RC32M_gc;
 }
 
-//  выбор опорного напряжения
-void XmegaAdc::xmega_adc_select_vref(ADC_t*adc,ADC_REFSEL_t ref_vcc)
+/*Выбор опорного напряжения
+ * \param  ADC_REFSEL_INT1V_gc Внутреннее опорное напряжение 1.00В
+ * \param  ADC_REFSEL_VCC_gc   Внутреннее напряжение VCC/1.6
+ * \param  ADC_REFSEL_AREFA_gc Внешнее опорное напряжение с вывода AREF порта А
+ * \param  ADC_REFSEL_AREFB_gc Внешнее опорное напряжение с вывода AREF порта B
+ */
+void XmegaAdc::xmega_adc_select_vref(ADC_t *adc, ADC_REFSEL_t ref_vcc)
 {
-    adc->REFCTRL=ref_vcc;
-    //ADCA.REFCTRL = ADC_REFSEL_INT1V_gc; // Внутреннее опорное напряжение 1.00В
-    // ADCA.REFCTRL = ADC_REFSEL_VCC_gc;   // Внутреннее напряжение VCC/1.6
-    // ADCA.REFCTRL = ADC_REFSEL_AREFA_gc; // Внешнее опорное напряжение с вывода AREF порта А
-    // ADCA.REFCTRL = ADC_REFSEL_AREFB_gc; // Внешнее опорное напряжение с вывода AREF порта B
+    adc->REFCTRL = ref_vcc;
+    
 }
 
-// запуск одиночного преобразования в нужном канале
-void XmegaAdc::xmega_adc_start_chanel(ADC_CH_t *ch)
+// Запуск одиночного преобразования в нужном канале
+//Example for ADCA 0 chanel: &ADCA.CH0
+void XmegaAdc::xmega_adc_single_start_chanel(ADC_CH_t *ch)
 {
     ch->CTRL = ADC_CH_START_bm;
 }
 
 void XmegaAdc::xmega_adc_init(ADC_t *adc, ADC_REFSEL_t ref, ADC_PRESCALER_t prescaler, ADC_CH_t *ch)
 {
-    //PORTA.DIRCLR = PIN0_bm; // configure PORTA PIN0 as input
+    // PORTA.DIRCLR = PIN0_bm; // configure PORTA PIN0 as input
 
-    adc->REFCTRL =ref;
+    adc->REFCTRL = ref;
     adc->PRESCALER = prescaler; //
-    ch->CTRL = ADC_CH_START_bm;    //выбор канала         
-    adc->CTRLA = ADC_ENABLE_bm;// включение ADC
-    
+    ch->CTRL = ADC_CH_START_bm; // выбор канала
+    adc->CTRLA = ADC_ENABLE_bm; // включение ADC
 }
 
 // запуск автоматического преобразования в нужном канале
-void XmegaAdc::xmega_adc_auto_mode(ADC_t *adc )
+void XmegaAdc::xmega_adc_multiple_mode(ADC_t *adc)
 {
     adc->CTRLB = ADC_FREERUN_bm; // включение многократного преобразования
     adc->EVCTRL = ADC_SWEEP0_bm; // выбор канала для автоматического преобразования: CH0
 }
 
 // Очистка конвеера АЦП.
-void XmegaAdc::xmega_adc_clear(ADC_t*adc)
+void XmegaAdc::xmega_adc_clear(ADC_t *adc)
 {
     adc->CTRLA = ADC_FLUSH_bm;
 }
